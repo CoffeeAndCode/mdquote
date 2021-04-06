bee78389f4bb4965daeec37f6209fc011fed0ae8
Jonathan Knapp <jon@coffeeandcode.com>
2021-04-05 20:42:38 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    naersk.url = "github:nmattia/naersk";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      # `nix build`
+      packages.mdquote = naersk-lib.buildPackage {
+        pname = "mdquote";
+        root = ./.;
+      };
+      defaultPackage = packages.mdquote;
+
+      # `nix run`
+      apps.mdquote = utils.lib.mkApp {
+        drv = packages.mdquote;
+      };
+      defaultApp = apps.mdquote;
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ cargo rustc ];
+      };
+    });
+}
